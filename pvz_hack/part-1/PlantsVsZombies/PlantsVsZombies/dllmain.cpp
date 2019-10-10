6a30ecc2dba7fdb1d8c2c0a7d5ba80d306eb1367
un4ckn0wl3z <slickerth@gmail.com>
2019-10-10 22:39:50 +0700

    add pvz hacker part 1

---
+++ pvz_hack/part-1/PlantsVsZombies/PlantsVsZombies/dllmain.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+
+__declspec(dllexport) BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBox(NULL, _T("DLL PROCESS ATTACH"), _T("DLL"), MB_OK | MB_ICONINFORMATION);
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
